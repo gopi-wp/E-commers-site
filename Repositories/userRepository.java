9b5af7e52c72e406cfb819420886ccb8bbcb560e
GopiBrahmahmaih <gopidonthula563@gmail.com>
2025-10-16 15:02:19 +0530

    Create userRepository.java

---
+++ Repositories/userRepository.java
@@ -0,0 +1,8 @@
+package com.example.ecommerce.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.ecommerce.model.User;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByUsername(String username);
+}
